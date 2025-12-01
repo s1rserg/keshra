4e8c8fc321ea2ac1c3a41ae54b0e53a901b777bb
s1rserg <sirserg05@gmail.com>
2025-12-01 17:53:38 +0200

    fix(video-calls): replace stun config by turn config for webrtc connection

--- src/pages/ChatsPage/hooks/useVideoCalls/config.ts
+++ src/pages/ChatsPage/hooks/useVideoCalls/config.ts
@@ -1,6 +1,30 @@
+const turnUsername = import.meta.env.VITE_TURN_USERNAME;
+const turnCredential = import.meta.env.VITE_TURN_CREDENTIAL;
+
 export const ICE_SERVERS = {
   iceServers: [
-    { urls: 'stun:stun.l.google.com:19302' },
-    { urls: 'stun:global.stun.twilio.com:3478' },
+    {
+      urls: 'stun:stun.relay.metered.ca:80',
+    },
+    {
+      urls: 'turn:standard.relay.metered.ca:80',
+      username: turnUsername,
+      credential: turnCredential,
+    },
+    {
+      urls: 'turn:standard.relay.metered.ca:80?transport=tcp',
+      username: turnUsername,
+      credential: turnCredential,
+    },
+    {
+      urls: 'turn:standard.relay.metered.ca:443',
+      username: turnUsername,
+      credential: turnCredential,
+    },
+    {
+      urls: 'turns:standard.relay.metered.ca:443?transport=tcp',
+      username: turnUsername,
+      credential: turnCredential,
+    },
   ],
 };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_API_BASE_URL: string;
   readonly VITE_SOCKET_BASE_URL: string;
+  readonly VITE_TURN_USERNAME: string;
+  readonly VITE_TURN_CREDENTIAL: string;
 }
 
 interface ImportMeta {
